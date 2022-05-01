8e05dab2c9fe7ec5e41ae89f7f0069ef4254ec79
ThameeraFernando <sithnuwan111@gmail.com>
2022-05-01 17:16:03 +0530

    add JWT setup

--- modals/User.js
+++ modals/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -22,6 +23,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
+    select: false,
   },
   lastName: {
     type: String,
@@ -36,10 +38,17 @@ const UserSchema = new mongoose.Schema({
     default: "myCity",
   },
 });
-//hash password before save the document 
+//hash password before save the document
 UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+//method for create a web token
+UserSchema.methods.createJWT = function () {
+  // console.log(this);
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 export default mongoose.model("User", UserSchema);
