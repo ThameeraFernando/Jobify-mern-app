0eedbb47b422566ed206d2c1ca0601b3402d96f3
ThameeraFernando <sithnuwan111@gmail.com>
2022-05-02 13:36:33 +0530

    add sharedlayout and protected routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,14 @@ import Error from "./pages/Error";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Register from "./pages/Register";
 // import DashBoard from "./pages/DashBoard";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Status,
+} from "./pages/dashboard";
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,7 +21,19 @@ const App = () => {
         <Link to="/landing">Home</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<div>DashBoard</div>}></Route>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Status />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/landing" element={<Landing />}></Route>
         <Route path="*" element={<Error />}></Route>
