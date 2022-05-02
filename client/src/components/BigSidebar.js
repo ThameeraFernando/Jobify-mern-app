f25bc1292bc1b76b8d209af6ae4b5bf4b8286b5f
ThameeraFernando <sithnuwan111@gmail.com>
2022-05-02 18:59:17 +0530

    add big side bar

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/BigSidebar";
-
+import Logo from "./logo";
+import NavLinks from "../utils/NavLinks";
+import { useAppContext } from "../Context/appContext";
 const BigSidebar = () => {
-  return <Wrapper>BigSidebar</Wrapper>;
+  const { showSidebar } = useAppContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default BigSidebar;
