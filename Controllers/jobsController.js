adef7d14ba7d952dafb74144de5bb66d9c26bcfc
ThameeraFernando <sithnuwan111@gmail.com>
2022-05-03 10:57:21 +0530

    add job modal and createjob controller to server

--- Controllers/jobsController.js
+++ Controllers/jobsController.js
@@ -1,15 +1,30 @@
+import Job from "../modals/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
+//create a job
 const createJob = async (req, res) => {
-  res.send("createJob .");
+  const { position, company } = req.body;
+  if (!position || !company) {
+    throw new BadRequestError("Please Provide all  values.");
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+//dele a job
 const deleteJob = async (req, res) => {
   res.send("deleteJob .");
 };
+//update a job
 const updateJob = async (req, res) => {
   res.send("updateJob .");
 };
+//get all jobs
 const getAllJobs = async (req, res) => {
   res.send("getAllJobs .");
 };
+//show status
 const showStatus = async (req, res) => {
   res.send("showStatus .");
 };
